393d1d5fe2c6cc8ddd3bbdbd046fe75b0299cc48
kquilty04 <62107807+kquilty04@users.noreply.github.com>
2020-10-27 17:03:38 +0900

    메인메뉴추가

---
+++ Assets/Units/메인메뉴/Scripts/FadeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    public Image Panel;
+    float time = 0f;
+    float F_time = 1f;
+    public void Fade()
+    {
+        StartCoroutine(FadeFlow());
+    }
+    IEnumerator FadeFlow()
+    {
+        Panel.gameObject.SetActive(true);
+        Color alpha = Panel.color;
+        while (alpha.a < 1f)
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        yield return null;
+    }
+}
