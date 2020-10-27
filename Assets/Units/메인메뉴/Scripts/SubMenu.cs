a9b770b23dc6e11cf0ec7ee3ffe26956fae6accb
kquilty04 <62107807+kquilty04@users.noreply.github.com>
2020-10-27 20:07:38 +0900

    서브메뉴 추가

---
+++ Assets/Units/메인메뉴/Scripts/SubMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubMenu : MonoBehaviour
+{
+    public GameObject GsubMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GsubMenu.activeSelf) GsubMenu.gameObject.SetActive(false);
+            else GsubMenu.gameObject.SetActive(true);
+        }
+
+    }
+
+    public void NoMoreGame()
+    {
+    #if UNITY_EDITOR
+      UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+
+}
