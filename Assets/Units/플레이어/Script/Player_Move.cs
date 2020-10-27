95e8d5e5452cbaa3c536b862b0633aa60feb9c50
Kim-DaeKyung <Kim-DaeKyung@gmail.com>
2020-10-27 17:33:34 +0900

    플레이어공격추가

--- Assets/Units/플레이어/Script/Player_Move.cs
+++ Assets/Units/플레이어/Script/Player_Move.cs
@@ -6,33 +6,52 @@ public class Player_Move : MonoBehaviour
 {
     public float speed;
     private Vector3 vector;
-    private Animator animator;
+    public Animator animator;
+    private SpriteRenderer sprite;
+    private float cooltime_attack = 0;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool press_left = Input.GetKey(KeyCode.LeftArrow);
-        bool press_right = Input.GetKey(KeyCode.RightArrow);
-        if (press_left || press_right)
+        if (cooltime_attack > 0) cooltime_attack -= Time.deltaTime;
+        animator.SetBool("isWalk", false);
+
+
+        if (Input.GetKeyDown(KeyCode.Q) && cooltime_attack <= 0)
         {
-            animator.SetBool("Running", true);
-            
-            if (press_right)
-            {
-                animator.SetBool("GoRight", true);
-                if (animator.GetCurrentAnimatorStateInfo(0).IsName("player_run_right")) transform.Translate(+speed, 0, 0);
-            }
-            if (press_left)
-            {
-                animator.SetBool("GoRight", false);
-                if (animator.GetCurrentAnimatorStateInfo(0).IsName("player_run_left")) transform.Translate(-speed, 0, 0);
-            }
+            animator.SetBool("Attack", true);
+            cooltime_attack = 1;
+
         }
-        else animator.SetBool("Running", false);
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            vector.Set(Input.GetAxisRaw("Horizontal"), transform.position.y, transform.position.z);
+            if (vector.x == 1)
+            {
+                animator.SetBool("isWalk", true);
+                sprite.flipX = false;
+                transform.Translate(vector.x * speed, 0, 0);
+
+
+            }
+            else if (vector.x == -1)
+            {
+                animator.SetBool("isWalk", true);
+                sprite.flipX = true;
+                transform.Translate(vector.x * speed, 0, 0);
+
+
+            }
+
+        }
+
     }
+
+    
 }
