11e2e7c26c6751024fa245aad18db91a68adc553
Kim-DaeKyung <Kim-DaeKyung@gmail.com>
2020-11-02 23:29:23 +0900

    체력바와 스테나마uI제작 및 추가

--- Assets/Units/Stats.cs
+++ Assets/Units/Stats.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    public int maxhp=100;//최대 체력 변수
-    public int curhp = 100;//기본적인 체력 변수
+    public int maxhp;//최대 체력 변수
+    public int curhp;//기본적인 체력 변수
     public float Def;//방어력 변수
     public int stemana;//스테마나(플레이어가 방어 했을때 일저량 감소)
     public float Speed;//스피드 변수
