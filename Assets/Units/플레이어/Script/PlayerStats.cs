afa7c52713ace4ef52152adeff1a9c78a381279d
Kim-DaeKyung <Kim-DaeKyung@gmail.com>
2020-11-02 23:14:13 +0900

    플레이어 스탯
    따로 스크립트를 만들었다.

---
+++ Assets/Units/플레이어/Script/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : Stats
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        maxhp = 100;//최대 체력 변수
+        curhp = 100;//기본적인 체력 변수
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
