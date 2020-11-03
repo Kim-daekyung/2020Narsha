16aa5fc940c6621ff4cca703d1b18f5d708357e4
Dev-Winty <lkj1688@daum.net>
2020-11-03 20:40:49 +0900

    적 몬스터 AI 추가
    적 몬스터 AI 추가함

---
+++ Assets/Units/적몬스터/Scripts/TrackingPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TrackingPlayer : MonoBehaviour
+{
+    public enum CurrentState
+    {
+        idle,
+        trace,
+        attack,
+        dead
+    };
+
+    public CurrentState curState = CurrentState.idle;
+
+    private Transform _transform;
+    private Transform playerTransform;
+    private NavMeshAgent nvAgent;
+
+    public float traceDist = 100.0f;
+    public float attackDist = 50.0f;
+
+    private bool isDead = false;
+
+    private void Start()
+    {
+        _transform = this.GetComponent<Transform>();
+        playerTransform = GameObject.Find("플레이어").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+
+        StartCoroutine(this.CheckState());
+        StartCoroutine(this.CheckStateForAction());
+    }
+
+    private IEnumerator CheckState()
+    {
+        while (!isDead)
+        {
+            yield return new WaitForSeconds(0.2f);
+
+            float dist = Vector3.Distance(playerTransform.position, _transform.position);
+
+            if (dist <= attackDist)
+            {
+                curState = CurrentState.attack;
+            }
+            else if (dist <= traceDist)
+            {
+                curState = CurrentState.trace;
+            }
+            else
+            {
+                curState = CurrentState.idle;
+            }
+
+            Debug.Log(curState);
+        }
+    }
+
+    private IEnumerator CheckStateForAction()
+    {
+        while (!isDead)
+        {
+            switch (curState)
+            {
+                case CurrentState.idle:
+                    break;
+
+                case CurrentState.trace:
+                    break;
+
+                case CurrentState.attack:
+                    break;
+            }
+            yield return null;
+        }
+    }
+}
