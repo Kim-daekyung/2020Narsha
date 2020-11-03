becd5c24038ac29f014f075b0758d4e9f906c625
kiwon <kiwon4472@naver.com>
2020-11-03 16:54:30 +0900

    스테이지1

---
+++ Assets/Units/맵/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float left_limit = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec3 = new Vector3();
+        vec3.x = player.position.x;
+        if (player.position.x < left_limit) vec3.x = left_limit;
+        vec3.y = player.position.y + 27.50f;
+        vec3.z  = player.position.z;
+        transform.position = vec3;
+    }
+}
