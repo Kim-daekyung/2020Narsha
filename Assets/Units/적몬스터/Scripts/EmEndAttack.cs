44167a54df3e6799d5d377f16094ac702706d32b
Dev-Winty <lkj1688@daum.net>
2020-11-11 13:30:24 +0900

    Re 적 몬스터 공격 수정
    Re 적 몬스터 공격 수정

---
+++ Assets/Units/적몬스터/Scripts/EmEndAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class EmEndAttack : StateMachineBehaviour
+{
+    private bool isAttack = false;
+    private PlayerStats playerStats = GameObject.Find("플레이어").GetComponent<PlayerStats>();
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Task task = new Task(() =>
+        {
+
+        });
+        isAttack = true;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //
+    //}
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        isAttack = false;
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
