26a386399b808d6b2917e5687577f864c6231ca9
Kim-DaeKyung <Kim-DaeKyung@gmail.com>
2020-11-11 20:49:13 +0900

    플레이어dead애니메이션구현

--- Assets/Units/적몬스터/Scripts/MonsterMove.cs
+++ Assets/Units/적몬스터/Scripts/MonsterMove.cs
@@ -3,24 +3,33 @@
 public class MonsterMove : MonoBehaviour
 {
     private PlayerStats player_stat = null;
-    public GameObject palyer = null;
+    public GameObject playergo = null;
+    public Player_Move player = null;
 
-    private void Awake()
+    private void Start()
     {
-        palyer = GameObject.Find("플레이어");
+        playergo = GameObject.Find("플레이어");
+        player = playergo.GetComponent < Player_Move >();
         player_stat = GameObject.Find("플레이어").GetComponent<PlayerStats>();
     }
 
     private void AttackPlayer()
     {
-        player_stat.curhp -= 10;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+       
+        if (cc && cc.gameObject.tag == "Player")
         {
             player_stat.curhp -= 10;
+            if (player_stat.curhp <= 0)
+            {
+                player.animator.Play("player_death");
+                Debug.Log("DEAD");
+            }
         }
+    }
+
+    Collider2D cc;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        cc = collision;
     }
 }
