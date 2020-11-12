9abe087ab65223cc6dc7fb7f46edb0b6e85f82e3
kquilty04 <62107807+kquilty04@users.noreply.github.com>
2020-11-12 10:14:36 +0900

    죽음화면
    플레이어 피가 0 이하가 되면 사망 메뉴 띄움

--- Assets/Units/메인메뉴/Scripts/SubMenu.cs
+++ Assets/Units/메인메뉴/Scripts/SubMenu.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
 
 public class SubMenu : MonoBehaviour
 {
@@ -9,11 +11,13 @@ public class SubMenu : MonoBehaviour
     public AudioSource audio;
 
     public GameObject GsubMenu;
+    public GameObject DMenu;
 
     private float SVol = 1f;
 
     private void Start()
     {
+        PAlive();
         SVol = PlayerPrefs.GetFloat("SVol", 1f);
         SVolume.value = SVol;
         audio.volume = SVolume.value;
@@ -27,30 +31,54 @@ public class SubMenu : MonoBehaviour
     }
 
     void Update()
-    {
+    { 
+
         SoundSlider();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            
             GameObject.Find("SoundManager").GetComponent<SoundManager>().PlaySound6();
-            if (GsubMenu.activeSelf) { 
-                GsubMenu.gameObject.SetActive(false);
-                GameObject.Find("플레이어").GetComponent<PlayerStats>().speed = 2.0f;
-                Time.timeScale = 1; } //활성화 -> 비활
-            else {
-                GsubMenu.gameObject.SetActive(true);
-                GameObject.Find("플레이어").GetComponent<PlayerStats>().speed = 0.0f;
-                Time.timeScale = 0; } //비활 -> 활성화
+                if (GsubMenu.activeSelf) //창이 떠 있으면
+                {
+                    GsubMenu.gameObject.SetActive(false);
+                    PAlive();
+                } //활성화 -> 비활
+                else if (GsubMenu.activeSelf == false)
+                {
+                    GsubMenu.gameObject.SetActive(true);
+                    PDead();
+                
+            } //비활 -> 활성화
         }
+        if (GameObject.Find("플레이어").GetComponent<PlayerStats>().curhp <= 0) {
+            PDead();
+            DMenu.gameObject.SetActive(true);
+        }
+    }
 
+    public void restart() {
+        DMenu.gameObject.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+
+    public void PDead()
+    {
+        GameObject.Find("플레이어").GetComponent<PlayerStats>().speed = 0.0f;
+        Time.timeScale = 0;
+    }
+
+    public void PAlive()
+    {
+        GameObject.Find("플레이어").GetComponent<PlayerStats>().speed = 2.0f;
+        Time.timeScale = 1;
     }
 
     public void SubMenuContinue()
     {
         GameObject.Find("SoundManager").GetComponent<SoundManager>().PlaySound6();
-        GameObject.Find("플레이어").GetComponent<PlayerStats>().speed = 2.0f;
-        
+        PAlive();      
         GsubMenu.gameObject.SetActive(false);
-        Time.timeScale = 1; //활성화 -> 비활
+        //활성화 -> 비활
     }
     public void NoMoreGame()
     {
