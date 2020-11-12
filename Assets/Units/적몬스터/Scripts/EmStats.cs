b479cb5e2a31d2a63a0175d2a26e986e78d223da
Dev-Winty <lkj1688@daum.net>
2020-11-12 09:02:59 +0900

    적 몬스터 죽는 모션 추가
    적 몬스터 스탯, 죽는 모션 추가

---
+++ Assets/Units/적몬스터/Scripts/EmStats.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EmStats : MonoBehaviour
+{
+    public int maxhp = 100;//최대 체력 변수
+    public int curhp = 100;//현재 체력 변수
+    public float Def = 100;//방어력 변수
+    public int maxstemana = 0;//최대스테마나(플레이어가 방어 했을때 일저량 감소)
+    public int curstemana = 0;//현재 스테마나
+    public int attackpower = 10;//공격력 변수
+    private bool isDead = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (curhp <= 0)
+        {
+            isDead = true;
+
+            animator.SetBool("isDead", true);
+        }
+    }
+}
