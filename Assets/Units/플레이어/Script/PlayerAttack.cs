ad96c90fa51e7f7dfb2e2c4503c40de7ec4de0d5
Dev-Winty <lkj1688@daum.net>
2020-11-17 20:37:45 +0900

    PlayerAttack Fixed
    PlayerAttack Fixed

--- Assets/Units/플레이어/Script/PlayerAttack.cs
+++ Assets/Units/플레이어/Script/PlayerAttack.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -11,31 +9,24 @@ public class PlayerAttack : MonoBehaviour
     public int attack = 0;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         emgo = GameObject.FindWithTag("Enermy");
         Monster = emgo.GetComponent<MonsterMove>();
-        em_stat = GameObject.FindWithTag("Enermy").GetComponent<EmStats>();
+        em_stat = emgo.GetComponent<EmStats>();
     }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Enermy")
-        {
-            AttackEnermy();
-        }
-    }
+
+    //    private void OnTriggerStay2D(Collider2D collision)
+    //    {
+    //        if (collision.gameObject.tag == "Enermy")
+    //        {
+    //            AttackEnermy();
+    //        }
+    //    }
+
+    [System.Obsolete]
     public void AttackEnermy()
     {
         em_stat.curhp -= 10;
-    }
-
-    private Collider2D ccc;
-
-   
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
