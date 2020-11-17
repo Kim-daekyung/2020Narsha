15a7b34de74e472ca6a5c46077fc2371232f6152
kquilty04 <62107807+kquilty04@users.noreply.github.com>
2020-11-17 21:15:32 +0900

    스테이지 매니저 (미구햔)

--- Assets/Units/맵/Scripts/CameraController.cs
+++ Assets/Units/맵/Scripts/CameraController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    var stg = GameObject.Find("스테이지 매니저").GetComponent<StageManager>();
+
     public Transform player;
-    public float left_limit = 0;
+    public float left_limit_s1 = 81; //_s1 추가
+    public float right_limit_s1 = 433; //수정
+    
+    public float left_limit_s2 = 0; //수정
+    public float right_limit_s2 = 0; //수정
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +26,12 @@ public class CameraController : MonoBehaviour
     {
         Vector3 vec3 = new Vector3();
         vec3.x = player.position.x;
-        if (player.position.x < left_limit) vec3.x = left_limit;
+
+        if (stg.stage == 1) {
+            if (player.position.x < left_limit_s1) vec3.x = left_limit_s1; //_s1 추가
+            else if (player.position.x >= right_limit_s1) vec3.x = right_limit_s1; //수정
+        }
+
         vec3.y = player.position.y + 27.50f;
         vec3.z  = player.position.z;
         transform.position = vec3;
---
+++ Assets/Units/메인메뉴/Scripts/StageManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        stage = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) stage += 1;
+        if (Input.GetKeyDown(KeyCode.DownArrow)) stage -= 1;
+
+
+    }
+}
