f5dd60f7f9ff4b1d8bddd4ce50e874edd7127f86
Dev-Winty <lkj1688@daum.net>
2020-11-18 16:58:12 +0900

    플레이어 공격 시 대상이 범위에 없을 때 NullException이 뜨는 문제를 수정함
    플레이어 공격 시 대상이 범위에 없을 때 NullException이 뜨는 문제를 수정함

--- Assets/Units/플레이어/Script/PlayerAttack.cs
+++ Assets/Units/플레이어/Script/PlayerAttack.cs
@@ -4,43 +4,60 @@ public class PlayerAttack : MonoBehaviour
 {
     private EmStats em_stat = null;
     private Animator animator = null;
+
     public GameObject emgo = null;
     public MonsterMove Monster = null;
     public EmAttack Monem = null;
     public int attack = 0;
 
-    // Start is called before the first frame update
-    private void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        emgo = GameObject.FindWithTag("Enermy");
-        Monster = emgo.GetComponent<MonsterMove>();
-        em_stat = emgo.GetComponent<EmStats>();
-        animator = emgo.GetComponent<Animator>();
+        if (collision.gameObject.tag.Equals("Enermy"))
+        {
+            emgo = collision.gameObject;
+            Monster = emgo.GetComponent<MonsterMove>();
+            em_stat = emgo.GetComponent<EmStats>();
+            animator = emgo.GetComponent<Animator>();
+        }
+        else
+        {
+            emgo = null;
+        }
     }
 
-    //    private void OnTriggerStay2D(Collider2D collision)
-    //    {
-    //        if (collision.gameObject.tag == "Enermy")
-    //        {
-    //            AttackEnermy();
-    //        }
-    //    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        emgo = null;
+        Monster = null;
+        em_stat = null;
+        animator = null;
+    }
 
     [System.Obsolete]
     public void AttackEnermy()
     {
-        em_stat.curhp -= 10;
-//        animator.SetTrigger("monster_dam");
-        animator.Play("monster_dam");
+        if (emgo != null)
+        {
+            em_stat.curhp -= 10;
+            animator.Play("monster_dam");
+        }
     }
+
     public void AttackEnermy2()
     {
-        em_stat.curhp -= 20;
-        animator.Play("monster_dam");
+        if (emgo != null)
+        {
+            em_stat.curhp -= 20;
+            animator.Play("monster_dam");
+        }
     }
+
     public void PullAttackEnermy()
     {
-        em_stat.curhp -= 30;
-        animator.Play("monster_dam");
+        if (emgo != null)
+        {
+            em_stat.curhp -= 30;
+            animator.Play("monster_dam");
+        }
     }
 }
